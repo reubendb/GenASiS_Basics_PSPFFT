ecb57885bd82f616ed19d290e7359ed32ee22be5
Reuben D. Budiardja <reubendb@ornl.gov>
2024-07-22 13:47:27 -0400

    Started porting HomoegenousSpheroid.

--- Programs/Examples/Basics/FluidDynamics/HomogeneousSpheroid_3D_Program.f90
+++ Programs/Examples/Basics/FluidDynamics/HomogeneousSpheroid_3D_Program.f90
@@ -2,11 +2,11 @@ program HomogeneousSpheroid_3D_Program
  
   use Basics
   use PSPFFT
-  use IO
+  !use IO
   
   implicit none
   
-  include 'mpif.h'
+  !include 'mpif.h'
 
   integer (KDI) :: &
     Error, &
@@ -28,106 +28,116 @@ program HomogeneousSpheroid_3D_Program
     VariableName
   character(LDF) :: &
     DataDirectory
-  type(IO_Form), pointer :: &
-    FileRead, &
-    FileWrite
-  type(QuadMeshForm), pointer :: &
-    Mesh
-  type(Real_3D_Form), dimension(3) :: &
-    AR
-  type(VariableArrayReal_3D_GroupBase), dimension(1) :: &
-    VAR
-  type(PSPFFT_Form), pointer :: &
-    PS
+  !type(IO_Form), pointer :: &
+  !  FileRead, &
+  !  FileWrite
+  !type(QuadMeshForm), pointer :: &
+  !  Mesh
+  !type(Real_3D_Form), dimension(3) :: &
+  !  AR
+  !type(VariableArrayReal_3D_GroupBase), dimension(1) :: &
+  !  VAR
+  !type(PSPFFT_Form), pointer :: &
+  !  PS
   
-  call MPI_INIT(Error)
+  !call MPI_INIT(Error)
   
-  call MPI_COMM_SIZE(MPI_COMM_WORLD, nProcs, Error)
-  write(nProcsString, fmt='(i2)') nProcs
-  nProcsRoot = nProcs**(1.0_KDR/3) + 0.5_KDR
+  !call MPI_COMM_SIZE(MPI_COMM_WORLD, nProcs, Error)
+  !write(nProcsString, fmt='(i2)') nProcs
+  !nProcsRoot = nProcs**(1.0_KDR/3) + 0.5_KDR
+  
+  allocate ( PROGRAM_HEADER )
+  call PROGRAM_HEADER % Initialize ( 'HomogeneousSpheroid' )
+  
+  write(nProcsString, fmt='(i2)') PROGRAM_HEADER % Communicator % Size
+  
   DataDirectory = 'Data/Data_'//trim(adjustl(nProcsString)) // 'proc/'
+  
+  call Show ( DataDirectory, 'DataDirectory' )
   
   VariableName = (/'Source                        ', &
                    'AnalyticalSolution            ', & 
                    'NumericalSolution             '/)
   
-  !-- VAR(1) uses AR as its storage, which at this point is unallocated 
-  call Initialize ( VAR(1), AR, VariableOption = VariableName )
-  
-  !-- Create IO objects read data file from, and write the solution to
-  call Create( &
-         FileRead, 'HomogeneousSpheroid_3D', MPI_COMM_Option = MPI_COMM_WORLD)
-  call Open( &
-         FileRead, FileRead%READ_ACCESS, &
-         WorkingDirectoryOption = trim(DataDirectory), NumberOption = 0)
-  call Create( &
-         FileWrite, 'HomogeneousSpheroid_3D', MPI_COMM_Option = MPI_COMM_WORLD)
-  call Open( &
-         FileWrite, FileWrite%WRITE_ACCESS, &
-         WorkingDirectoryOption = trim(DataDirectory), &
-         SeriesOption = .true., NumberOption = 1)
-  
-  !-- Create Mesh object then read mesh & variable. The mesh is uses VAR
-  !   to store its value (which in turns points to AR as the real storage) 
-  !   Storage in AR is allocated during the Read() method as requested by its 
-  !   last argument.
-  call Create(Mesh, VAR)
-  call Modify(Mesh, Directory = '/', Name = '')
-  call Read(Mesh, FileRead, AllocateStorageOption = .true.)
-  
-  !-- Copy the "Source" the "NumericalSolution" because our solver solves
-  !   the Poisson equation in-place:
-  !-- VAR(1)%Data(1)%Data is a rank 3 array with the index i,j,k
-  !   gives the value of the source on mesh grid i,j,k
-  
-  Pi = acos(-1.0_KDR)
-  VAR(1)%Data(3)%Data &
-    = VAR(1)%Data(1)%Data * 4.0_KDR * Pi * GravitationalConstant
-  
-  !-- Create Poisson Solver Object
-  CellWidth(1) = Mesh%NodeCoordinate_1(2) - Mesh%NodeCoordinate_1(1)
-  CellWidth(2) = Mesh%NodeCoordinate_2(2) - Mesh%NodeCoordinate_2(1)
-  CellWidth(3) = Mesh%NodeCoordinate_3(2) - Mesh%NodeCoordinate_3(1)
-  nTotalCells = Mesh%VariableArrayGroup_3D(1)%nData(1)%Data * nProcsRoot
-  call Create(PS, CellWidth,  nTotalCells, MPI_COMM_WORLD)
-  
-  !-- Solve the Poisson Equations with Solve() subroutine, passing an array
-  !   of sources as argument. Although currently only array of size 1 is 
-  !   supported, this is done for future compatibility where multiple sources 
-  !   may be solved at once. 
-  !   Source is replaced by Solution upon subroutine exit 
-  
-  call Solve(PS, VAR(1)%Data(3:3))
-  
-  !-- Calculate L1 norm of relative error between Analytical Solution
-  !-- and Numerical solution
-  
-  MyAnalyticalSum = sum(abs(VAR(1)%Data(2)%Data))
-  MyDifferenceSum = sum(abs(VAR(1)%Data(3)%Data - VAR(1)%Data(2)%Data))
-  call Reduce( &
-         MyAnalyticalSum, FileRead%Communicator, REDUCTION_SUM, 0, AnalyticalSum)
-  call Reduce( &
-         MyDifferenceSum, FileRead%Communicator, REDUCTION_SUM, 0, DifferenceSum)
-  L1_Norm = DifferenceSum / AnalyticalSum
-  call Show(L1_Norm, 'L1_Norm Error')
-  
-  !-- Write the mesh with the Solution (i.e. potential) for the source
-  
-  call Write(Mesh, FileWrite)
-  
-  !-- Cleanup
-  call Destroy(PS)
-  
-  call Destroy(Mesh)
-  
-  call Close(FileWrite)
-  call Destroy(FileWrite)
-  
-  call Close(FileRead)
-  call Destroy(FileRead)  
-  
-  call Finalize(VAR(1))
-  
-  call MPI_FINALIZE(Error)
+!--  !-- VAR(1) uses AR as its storage, which at this point is unallocated 
+!--  call Initialize ( VAR(1), AR, VariableOption = VariableName )
+!--  
+!--  !-- Create IO objects read data file from, and write the solution to
+!--  call Create( &
+!--         FileRead, 'HomogeneousSpheroid_3D', MPI_COMM_Option = MPI_COMM_WORLD)
+!--  call Open( &
+!--         FileRead, FileRead%READ_ACCESS, &
+!--         WorkingDirectoryOption = trim(DataDirectory), NumberOption = 0)
+!--  call Create( &
+!--         FileWrite, 'HomogeneousSpheroid_3D', MPI_COMM_Option = MPI_COMM_WORLD)
+!--  call Open( &
+!--         FileWrite, FileWrite%WRITE_ACCESS, &
+!--         WorkingDirectoryOption = trim(DataDirectory), &
+!--         SeriesOption = .true., NumberOption = 1)
+!--  
+!--  !-- Create Mesh object then read mesh & variable. The mesh is uses VAR
+!--  !   to store its value (which in turns points to AR as the real storage) 
+!--  !   Storage in AR is allocated during the Read() method as requested by its 
+!--  !   last argument.
+!--  call Create(Mesh, VAR)
+!--  call Modify(Mesh, Directory = '/', Name = '')
+!--  call Read(Mesh, FileRead, AllocateStorageOption = .true.)
+!--  
+!--  !-- Copy the "Source" the "NumericalSolution" because our solver solves
+!--  !   the Poisson equation in-place:
+!--  !-- VAR(1)%Data(1)%Data is a rank 3 array with the index i,j,k
+!--  !   gives the value of the source on mesh grid i,j,k
+!--  
+!--  Pi = acos(-1.0_KDR)
+!--  VAR(1)%Data(3)%Data &
+!--    = VAR(1)%Data(1)%Data * 4.0_KDR * Pi * GravitationalConstant
+!--  
+!--  !-- Create Poisson Solver Object
+!--  CellWidth(1) = Mesh%NodeCoordinate_1(2) - Mesh%NodeCoordinate_1(1)
+!--  CellWidth(2) = Mesh%NodeCoordinate_2(2) - Mesh%NodeCoordinate_2(1)
+!--  CellWidth(3) = Mesh%NodeCoordinate_3(2) - Mesh%NodeCoordinate_3(1)
+!--  nTotalCells = Mesh%VariableArrayGroup_3D(1)%nData(1)%Data * nProcsRoot
+!--  call Create(PS, CellWidth,  nTotalCells, MPI_COMM_WORLD)
+!--  
+!--  !-- Solve the Poisson Equations with Solve() subroutine, passing an array
+!--  !   of sources as argument. Although currently only array of size 1 is 
+!--  !   supported, this is done for future compatibility where multiple sources 
+!--  !   may be solved at once. 
+!--  !   Source is replaced by Solution upon subroutine exit 
+!--  
+!--  call Solve(PS, VAR(1)%Data(3:3))
+!--  
+!--  !-- Calculate L1 norm of relative error between Analytical Solution
+!--  !-- and Numerical solution
+!--  
+!--  MyAnalyticalSum = sum(abs(VAR(1)%Data(2)%Data))
+!--  MyDifferenceSum = sum(abs(VAR(1)%Data(3)%Data - VAR(1)%Data(2)%Data))
+!--  call Reduce( &
+!--         MyAnalyticalSum, FileRead%Communicator, REDUCTION_SUM, 0, AnalyticalSum)
+!--  call Reduce( &
+!--         MyDifferenceSum, FileRead%Communicator, REDUCTION_SUM, 0, DifferenceSum)
+!--  L1_Norm = DifferenceSum / AnalyticalSum
+!--  call Show(L1_Norm, 'L1_Norm Error')
+!--  
+!--  !-- Write the mesh with the Solution (i.e. potential) for the source
+!--  
+!--  call Write(Mesh, FileWrite)
+!--  
+!--  !-- Cleanup
+!--  call Destroy(PS)
+!--  
+!--  call Destroy(Mesh)
+!--  
+!--  call Close(FileWrite)
+!--  call Destroy(FileWrite)
+!--  
+!--  call Close(FileRead)
+!--  call Destroy(FileRead)  
+!--  
+!--  call Finalize(VAR(1))
+!--  
+!--  call MPI_FINALIZE(Error)
+
+  deallocate ( PROGRAM_HEADER )
   
 end program HomogeneousSpheroid_3D_Program
