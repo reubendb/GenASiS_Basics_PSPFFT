25d5d0b13c9d5a855778997a1a411c095e8e5c48
Alaina Edwards <17e@ornl.gov>
2024-08-05 14:24:37 -0400

    Including UnitTests for PSPFFT --WIP

---
+++ Programs/UnitTests/PSPFFT/LaplacianIsolated_FFT__Form_Test.f90
@@ -0,0 +1,18 @@
+program LaplacianIsolated_FFT__Form_Test
+
+ ! use Specifiers
+ ! use DataManagement
+ ! use MessagePassing
+ ! use PSPFFT
+  use LaplacianIsolated_FFT__Form
+  
+  implicit none 
+
+  print *
+  print *, 'Laplacian Isolated FFT Test'
+  print * 
+
+ 
+
+end program LaplacianIsolated_FFT__Form_Test
+
---
+++ Programs/UnitTests/PSPFFT/Transpose_Command_Test.f90
@@ -0,0 +1,15 @@
+program Transpose_Command_Test
+
+  use Basics
+  use PSPFFT
+  use Transpose_Command
+  
+  implicit none 
+
+  print *
+  print *, 'Transpose Test'
+  print * 
+
+ 
+
+end program Transpose_Command_Test
